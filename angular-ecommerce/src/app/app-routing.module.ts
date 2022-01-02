ba9c038872d37d0274f5baa55083a14ef3eb67b7
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-01-03 05:04:45 +0900

    コードの整理をいくつか

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router'
 import { ProductListComponent } from './components/product/product-list/product-list.component'
 import { ProductDetailComponent } from './components/product/product-detail/product-detail.component'
 
+/**
+ * GET /products                           - get all products (with pagination)
+ * GET /products/00                        - get a single product
+ * GET /products?categoryId=00             - get all products with the category
+ * GET /products?categoryId=00&keyword=XXX - get products which matches the given keyword
+ *
+ */
 const routes: Routes = [
   { path: 'products/:id', component: ProductDetailComponent },
-  // all products -> /products
-  // with categories -> /products?categoryId=1
-  // with keywords -> /products?categoryId=1&keyword=python
   { path: 'products', component: ProductListComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', redirectTo: '/products' },
