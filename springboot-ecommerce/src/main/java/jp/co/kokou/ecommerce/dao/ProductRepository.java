265bf31af86502aba1039c02453175ab45d4365d
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-01-02 11:17:57 +0900

    Swagger サポートを追加する

--- springboot-ecommerce/src/main/java/jp/co/kokou/ecommerce/dao/ProductRepository.java
+++ springboot-ecommerce/src/main/java/jp/co/kokou/ecommerce/dao/ProductRepository.java
@@ -3,10 +3,14 @@ package jp.co.kokou.ecommerce.dao;
 import jp.co.kokou.ecommerce.entities.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryId(@Param("categoryId") Long id, Pageable pageable);
+
+    @Query("select p from Product p where p.name like ?1 and p.category.id = ?2")
+    List<Product> queryByKeyword(@Param("name") String name, @Param("categoryId") Long id, Pageable pageable);
 }
